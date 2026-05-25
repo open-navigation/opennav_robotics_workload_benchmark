fc7a9629ad3978c8eca9805679f13b2a39d483b9
SteveMacenski <stevenmacenski@gmail.com>
2026-05-25 14:29:31 -0700

    adding the vlm camera to simulation
    Signed-off-by: SteveMacenski <stevenmacenski@gmail.com>

--- opennav_benchmark_ai/opennav_benchmark_vlm/launch/vlm.launch.py
+++ opennav_benchmark_ai/opennav_benchmark_vlm/launch/vlm.launch.py
@@ -13,38 +13,12 @@ def generate_launch_description():
         'vlm_params.yaml',
     ])
 
-    base_url = LaunchConfiguration('base_url')
-    model = LaunchConfiguration('model')
-    default_image_topic = LaunchConfiguration('default_image_topic')
-
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'base_url',
-            default_value='http://localhost:8080/v1',
-            description='Endpoint of the local VLM server.',
-        ),
-        DeclareLaunchArgument(
-            'model',
-            default_value='gemma-4',
-            description='Model label.',
-        ),
-        DeclareLaunchArgument(
-            'default_image_topic',
-            default_value='/camera/rgb/image',
-            description='Image topic to use for requests that omit the image field.',
-        ),
         Node(
             package='opennav_benchmark_vlm',
             executable='vlm_node',
             name='vlm_node',
             output='screen',
-            parameters=[
-                params_file,
-                {
-                    'base_url': base_url,
-                    'model': model,
-                    'default_image_topic': default_image_topic,
-                },
-            ],
+            parameters=[params_file],
         ),
     ])
